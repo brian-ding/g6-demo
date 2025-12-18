c269cbf6eda22bcded63c962f4e9afc7e6410833
Brian Ding <brian_ding@outlook.com>
2025-12-18 21:43:00 +0800

    use azure image node

--- app/g6/page.tsx
+++ app/g6/page.tsx
@@ -1,35 +1,64 @@
 'use client';
 
 import { Graph } from '@antv/g6';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default () => {
     const containerRef = useRef<HTMLDivElement>(null);
+    const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
+        setIsClient(true);
+
         const graph = new Graph({
             container: containerRef.current!,
-            layout: { type: 'dagre' },
-            width: 500,
-            height: 500,
+            layout: {
+                type: 'dagre',
+                rankdir: 'LR',
+                align: 'UR',
+                nodesep: 100,
+                ranksep: 150,
+            },
+            // layout: { type: 'random' },
+            width: 1500,
+            height: 1000,
             data: {
                 nodes: [
-                    {
-                        id: 'node-1',
-                        // style: { x: 50, y: 100 },
-                    },
-                    {
-                        id: 'node-2',
-                        // style: { x: 150, y: 100 },
-                    },
-                    {
-                        id: 'node-3',
-                    }
+                    { id: 'gwy', type: 'image', style: { size: 80, src: '/icons/10042-icon-service-API-Management-Services.svg' }, },
+                    { id: 'svc1', type: 'image', style: { size: 80, src: '/icons/10035-icon-service-App-Services.svg' }, },
+                    { id: 'svc2', type: 'image', style: { size: 80, src: '/icons/10035-icon-service-App-Services.svg' }, },
+                    { id: 'svc3', type: 'image', style: { size: 80, src: '/icons/10035-icon-service-App-Services.svg' }, },
+                    { id: 'sb', type: 'image', style: { size: 80, src: '/icons/10836-icon-service-Azure-Service-Bus.svg' }, },
+                    { id: 'db1', type: 'image', style: { size: 80, src: '/icons/10130-icon-service-SQL-Database.svg' }, },
+                    { id: 'db2', type: 'image', style: { size: 80, src: '/icons/10130-icon-service-SQL-Database.svg' }, },
+                    // {
+                    //     id: 'active',
+                    //     states: ['active'],
+                    // },
+                    // {
+                    //     id: 'selected',
+                    //     states: ['selected'],
+                    // },
+                    // {
+                    //     id: 'highlight',
+                    //     states: ['highlight'],
+                    // },
+                    // {
+                    //     id: 'inactive',
+                    //     states: ['inactive'],
+                    // },
+                    // {
+                    //     id: 'disabled',
+                    //     states: ['disabled'],
+                    // },
                 ],
                 edges: [
-                    { id: 'edge-1', source: 'node-1', target: 'node-2' },
-                    { id: 'edge-2', source: 'node-1', target: 'node-3' },
-                    { id: 'edge-3', source: 'node-1', target: 'node-3' }
+                    { id: 'edge1', source: 'gwy', target: 'svc1', style: { lineWidth: 10, lineDash: [5, 5], endArrow: true } },
+                    { id: 'edge2', source: 'svc1', target: 'svc2' },
+                    { id: 'edge3', source: 'svc1', target: 'svc3' },
+                    { id: 'edge4', source: 'svc2', target: 'sb' },
+                    { id: 'edge5', source: 'svc2', target: 'db2' },
+                    { id: 'edge6', source: 'svc3', target: 'db1' },
                 ],
             },
         });
