9770fb2c3fb96cd49c811986ef00bd8b5a23d10c
Brian Ding <brian_ding@outlook.com>
2025-12-20 20:55:34 +0800

    use layout

--- app/arch/page.tsx
+++ app/arch/page.tsx
@@ -28,10 +28,33 @@ export default () => {
         });
 
         graph.render();
-        // graph.fitCenter();
     }, []);
 
-    return (<div ref={containerRef} style={{ width: '100%', height: '100vh' }}
-    />
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100vh',
+            background: 'grey',
+        }}>
+            <div style={{
+                flexGrow: 0,
+                background: '#BB6611',
+            }}>top menu</div>
+            <div style={{
+                display: 'flex',
+                flexGrow: 1,
+            }}>
+                <div ref={containerRef} style={{
+                    flexGrow: 1,
+                    background: '#567843',
+                }}></div>
+                <div style={{
+                    flexGrow: 0,
+                    background: '#123456',
+                    color: '#FFFFFF',
+                }}>side bar</div>
+            </div>
+        </div>
     );
 };
